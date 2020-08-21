47839c02d3e2b0d844f77bca0b2bc96d5abc1ade
Aymen Hammami <vor@live.com>
2020-08-21 16:19:12 +0200

    Marque filter working automatic get marques for each category

--- src/DashBundle/Controller/AuthController.php
+++ src/DashBundle/Controller/AuthController.php
@@ -13,38 +13,79 @@ class AuthController extends Controller
 {
     public function loginAction(Request $request){
         if (($request->isMethod('POST'))){
-            $em = $this->getDoctrine()->getManager();
-            $clients = $em->getRepository('DashBundle:Client');
-            $login = $request->get('login');
-            $pass = $request->get('pass');
-            $clientO = $clients->findOneBy(array('codeClient' => $login));
             $session = $request->getSession();
             if (!($session->has('client'))){
                 $session->set('client', []);
             }
-
+            if (!($session->has('Auth'))){
+                $session->set('Auth', []);
+            }
+            $authSession = $session->get('Auth');
+            $em = $this->getDoctrine()->getManager();
+            $clients = $em->getRepository('DashBundle:Client');
+            $step = $request->request->get('step');
+            $codeClient = $request->request->get('codeClient');
+            if ($step != 'codeClient'){
+                $clientO = $clients->findOneBy(array('codeClient' => $authSession['codeClient']));
+            }else{
+                $clientO = $clients->findOneBy(array('codeClient' => $codeClient));
+            }
             //check If client Exist
             if (empty($clientO)){
                 return new JsonResponse('Account Not Exist');
-            }elseif ($clientO->getPassword() != $pass){
-                return new JsonResponse('Wrong Password');
-            }
+            }elseif (!empty($clientO) && $step == 'codeClient'){
+                if ($clientO->getVerified() == 1){
+                    $emailinput  =
+                        <<<EOD
+                    <div class="alert alert-danger" id="msg-error" style="display: none;" role="alert">Mot de passe incorrect!</div>
+                    <div class="form-group" id="passClient"><label for="singin-client-login">Entrer votre mot de passe *</label><input type="password" class="form-control" id="singin-client-login" name="singin-pass-client" data-step="passClient" required></div>
+EOD;
+                }else{
+                    $emailinput =
+                        <<<EOD
+                    <div class="form-group" id="emailClient"><label for="singin-client-login">Votre Adresse Email *</label><input type="email" class="form-control" id="singin-client-login" name="singin-email-client" data-step="emailClient" required></div>
+EOD;
+                }
 
-            $authClient = array();
-            $frmArray = [
-                'status' =>'in',
-                'tclient'   => $clientO->getCodeClient(),
-                'tnom'   => $clientO->getFullname(),
-                'tadresse'   => $clientO->getAddresse(),
-                'tphone'  => $clientO->getPhone(),
-                'tfax'  => $clientO->getFax(),
-                'tmatFiscale' => $clientO->getMfiscal(),
-                'password' => $clientO->getPassword(),
-                'verification' => $clientO->getVerified()
-            ];
-            $session->set('client', $frmArray);
-            $client = $session->get('client');
-            return new JsonResponse($client);
+                $resp = ['loginInput' => $emailinput, 'status'=> 'emailInput'];
+                $authSession['codeClient'] = $codeClient;
+                $session->set('Auth', $authSession);
+                return new JsonResponse($resp);
+            }elseif (!empty($clientO) && $step == 'emailClient'){
+                $clientO->setEmail($codeClient);
+                $clientO->setVerified(1);
+                $em->persist($clientO);
+                $em->flush();
+                $authSession['emailClient'] = $codeClient;
+                $session->set('Auth', $authSession);
+                //send email to the client with the current password
+
+                $successMsg =
+                    <<<EOD
+                    <div class="alert alert-success" role="alert">s'il vous plaît vérifier votre email pour obtenir votre mot de passe.</div>
+                    <div class="form-group" id="passClient"><label for="singin-client-login">Entrer votre mot de passe *</label><input type="password" class="form-control" id="singin-client-login" name="singin-pass-client" data-step="passClient" required></div>
+EOD;
+                return new JsonResponse(array('loginInput' => $successMsg, 'status' => 'successMsg'));
+            }elseif (!empty($clientO) && $step == 'passClient'){
+                if ($clientO->getPassword() == $codeClient){
+                    $authClient = array();
+                    $frmArray = [
+                        'status' =>'in',
+                        'tclient'   => $clientO->getCodeClient(),
+                        'tnom'   => $clientO->getFullname(),
+                        'tadresse'   => $clientO->getAddresse(),
+                        'tphone'  => $clientO->getPhone(),
+                        'tfax'  => $clientO->getFax(),
+                        'tmatFiscale' => $clientO->getMfiscal(),
+                        'password' => $clientO->getPassword(),
+                        'verification' => $clientO->getVerified()
+                    ];
+                    $session->set('client', $frmArray);
+                    $client = $session->get('client');
+                    return new JsonResponse(array('status' => 'in'));
+                }
+                return new JsonResponse(array('status' => 'smerf'));
+            }
         }
         return $this->render('@Front/pages/login.html.twig');
     }
--- web/assets/FrontBundle/js/cotupap/login.js
+++ web/assets/FrontBundle/js/cotupap/login.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function(e){
+    // e.preventDefault();
     $('#login').on( 'click', function () {
-        // var data = $(this).data();
-        var login = $('#singin-email').val();
-        var password = $('#singin-password').val();
+        var client = $('#singin-client-login');
         $.ajax({
             url: $('#loginform').data('url'),
             type: "POST",
             dataType: "json",
+            cache: false,
             data: {
-                "login": login,
-                "pass": password
+                "codeClient": client.val(),
+                "step": client.data('step')
             },
             async: true,
             success: function (data)
             {
                 // console.log(data['status']);
-                if (data['status'] === 'in'){
-                    console.log('done');
-                }else {
-                    console.log('not done');
+                if (data['status'] == 'smerf'){
+                    console.log(data['status']);
+                    $('#msg-error').css("display", "block");
+                } else if (data['status'] != 'in'){
+                    $('#content-side').html(data['loginInput'])
+                } else {
+                    window.location.reload();
                 }
             }
         });
